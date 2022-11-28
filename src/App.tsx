8a54a729d1b68691f8e8fc6fe23743728089580a
Wendreo Matheus <wendreo.pvh@gmail.com>
2022-11-28 08:41:26 -0300

    added provider and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Provider } from "jotai";
+import { Suspense } from "react";
 import TodoList from "./components/todo-list";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <TodoList />
-    </div>
+    <Provider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <TodoList />
+        </div>
+      </Suspense>
+    </Provider>
   );
 }
 
